bccda3486207bc2d391cd84b95a9183669e5bbb6
Jhon Vega <jhonvegamas@gmail.com>
2017-05-23 22:39:01 -0500

    Change unit test

--- test.php
+++ test.php
@@ -1,6 +1,14 @@
 <?php
-
-echo file_get_contents('http://127.0.0.1');
+function get_file_dir() {
+    global $argv;
+    $dir = dirname(getcwd() . '/' . $argv[0]);
+    $curDir = getcwd();
+    chdir($dir);
+    $dir = getcwd();
+    chdir($curDir);
+    return $dir;
+}
+print_r(get_file_dir('http://127.0.0.1'));
 echo "\n";
-echo file_get_contents('http://127.0.0.1/phpunit_codeigniter');
+print_r(get_file_dir('http://127.0.0.1/phpunit_codeigniter'));
 echo "\n";
