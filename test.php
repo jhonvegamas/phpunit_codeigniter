4658be044271aa430daa0cfd8c20a241d3c2bc56
Jhon Vega <jhonvegamas@gmail.com>
2017-05-23 23:50:42 -0500

    Change unit test

--- test.php
+++ test.php
@@ -1,7 +1,4 @@
 <?php
-$opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n"));
-$context = stream_context_create($opts);
-
 function get_file_dir() {
     global $argv;
     $dir = dirname(getcwd() . '/' . $argv[0]);
@@ -12,7 +9,7 @@ function get_file_dir() {
     return $dir;
 }
 echo "______________________________________________________________ \n";
-echo file_get_contents('http://127.0.0.1/phpunit_codeigniter/index.php/phpunit/teamTest/eliminar_equipo',false,$context);
+echo file_get_contents('http://127.0.0.1/phpunit_codeigniter/index.php/phpunit/teamTest/eliminar_equipo');
 echo "______________________________________________________________ \n";
 print_r(get_file_dir('http://127.0.0.1/phpunit_codeigniter/index.php/phpunit/teamTest/eliminar_equipo'));
 echo "\n";
