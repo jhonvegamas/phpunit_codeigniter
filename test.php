e92429abaf612e382ea97fa2dad8cbd19cd3766c
Jhon Vega <jhonvegamas@gmail.com>
2017-05-23 23:16:24 -0500

    Change unit test

--- test.php
+++ test.php
@@ -8,14 +8,9 @@ function get_file_dir() {
     chdir($curDir);
     return $dir;
 }
-echo file_get_contents('http://127.0.0.1/index.php/phpunit/teamTest/eliminar_equipo');
-echo "\n";
+echo "______________________________________________________________ \n";
 echo file_get_contents('http://127.0.0.1/phpunit_codeigniter/index.php/phpunit/teamTest/eliminar_equipo');
-echo "\n";
-echo file_get_contents('http://127.0.0.1/phpunit_codeigniter/phpunit/teamTest/eliminar_equipo');
-echo "\n";
-echo file_get_contents('http://127.0.0.1/phpunit/teamTest/eliminar_equipo');
-echo "\n";
+echo "______________________________________________________________ \n";
 print_r(get_file_dir('http://127.0.0.1'));
 echo "\n";
 print_r(get_file_dir('http://127.0.0.1/phpunit_codeigniter'));
