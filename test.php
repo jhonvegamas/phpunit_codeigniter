d1c429252fd9b0d19e2f0d0f180cba5dcd3c5899
Jhon Vega <jhonvegamas@gmail.com>
2017-05-23 22:09:01 -0500

    Change unit test

--- test.php
+++ test.php
@@ -1,4 +1,6 @@
 <?php
 
-file_get_contents('http://127.0.0.1');
-file_get_contents('http://127.0.0.1/phpunit_codeigniter');
+echo file_get_contents('http://127.0.0.1');
+echo "\n";
+echo file_get_contents('http://127.0.0.1/phpunit_codeigniter');
+echo "\n";
