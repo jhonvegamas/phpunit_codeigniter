1cf0d50007223b587ffbf1225113d2b629dc3514
Jhon Vega <jhonvegamas@gmail.com>
2017-05-29 19:44:21 -0500

    Reestructuración de las pruebas

--- application/views/index.php
+++ application/views/index.php
@@ -1,3 +1,8 @@
-<<?php
-echo 'hello';
- ?>
+<html>
+<head>
+<meta http-equiv="Refresh" content="5;url=https://www.arkadev.me">
+</head>
+<body>
+<p>Nos hemos mudado! Serás dirigido automáticamente en cinco segundos. En caso contrario, puedes acceder haciendo click <a href="https://www.arkadev.me">aquí</a></p>
+</body>
+</html>
